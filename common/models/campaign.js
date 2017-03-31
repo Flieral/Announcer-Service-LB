b13c074c7caf07ff240a986f8be6e6f95aca2b5c
mohammad4x <m.ferdosi94@gmail.com>
2017-04-01 01:44:01 +0430

    Implement Campaign Validations

---
+++ common/models/campaign.js
@@ -0,0 +1,25 @@
+var methodDisabler = require('../../public/methodDisabler.js')
+var relationMethodPrefixes = [
+  'createChangeStream',
+  'upsertWithWhere',
+  'patchOrCreate',
+  'exists',
+  'prototype.patchAttributes'
+]
+
+module.exports = function (campaign) {
+  //methodDisabler.disableOnlyTheseMethods(placement, relationMethodPrefixes)
+  campaign.beforeRemote('prototype.__create__campaigns', function (ctx, modelInstance, next) {
+    if (!ctx.args.options.accessToken)
+      return next()
+    ctx.args.data.clientId = ctx.args.options.accessToken.userId
+    next()
+  })
+
+  campaign.beforeRemote('prototype.__updateById__campaigns', function (ctx, modelInstance, next) {
+    if (!ctx.args.options.accessToken)
+      return next()
+    ctx.args.data.clientId = ctx.args.options.accessToken.userId
+    next()
+  })
+}
