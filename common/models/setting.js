4dafef0b594894c44374af33cabde63f801f2a70
MrWooJ <hurricanc@gmail.com>
2017-04-05 22:54:20 +0430

    Add Setting Validation Methods and Static Configurations

--- common/models/setting.js
+++ common/models/setting.js
@@ -1,12 +1,20 @@
 var categoryList = require('../../config/category.json')
 var countryList = require('../../config/country.json')
 var userLabelList = require('../../config/userLabel.json')
+var priorityList = require('../../config/priority.json')
+var languageList = require('../../config/language.json')
+var osList = require('../../config/operatingSystem.json')
+var connectionList = require('../../config/connection.json')
+var deviceList = require('../../config/device.json')
 
 module.exports = function (setting) {
-
-  // Should Add Some Validation Methods
 
   setting.validatesInclusionOf('category', { in: categoryList })
   setting.validatesInclusionOf('country', { in: countryList })
   setting.validatesInclusionOf('userLabel', { in: userLabelList })
+  setting.validatesInclusionOf('priority', { in: priorityList })
+  setting.validatesInclusionOf('language', { in: languageList })
+  setting.validatesInclusionOf('os', { in: osList })
+  setting.validatesInclusionOf('connection', { in: connectionList })
+  setting.validatesInclusionOf('device', { in: deviceList })
 }
