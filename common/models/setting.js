260f4f2cbf4040786ccb54520424b24d4de2daed
MrWooJ <hurricanc@gmail.com>
2017-04-11 14:12:19 +0430

    Move Logics of Setting Create/Update Because of Prototype

--- common/models/setting.js
+++ common/models/setting.js
@@ -1,52 +1,6 @@
-var categoryList = require('../../config/category.json')
-var countryList = require('../../config/country.json')
-var userLabelList = require('../../config/userLabel.json')
 var priorityList = require('../../config/priority.json')
-var languageList = require('../../config/language.json')
-var osList = require('../../config/operatingSystem.json')
-var connectionList = require('../../config/connection.json')
-var deviceList = require('../../config/device.json')
-
-var utility = require('../../public/utility.js')
 
 module.exports = function (setting) {
 
   setting.validatesInclusionOf('priority', { in: priorityList })
-
-  setting.beforeRemote('create', function (ctx, modelInstance, next) {
-    if (!utility.JSONIterator(ctx.args.data.category, categoryList))
-      next(new Error('category Validation Error'))
-    if (!utility.JSONIterator(ctx.args.data.userLabel, userLabelList))
-      next(new Error('userLabel Validation Error'))
-    if (!utility.JSONIterator(ctx.args.data.country, countryList))
-      next(new Error('country Validation Error'))
-    if (!utility.JSONIterator(ctx.args.data.language, languageList))
-      next(new Error('language Validation Error'))
-    if (!utility.JSONIterator(ctx.args.data.os, osList))
-      next(new Error('os Validation Error'))
-    if (!utility.JSONIterator(ctx.args.data.connection, deviceList))
-      next(new Error('connection Validation Error'))
-    if (!utility.JSONIterator(ctx.args.data.device, deviceList))
-      next(new Error('device Validation Error'))
-    next()
-  })
-
-  setting.beforeRemote('update', function (ctx, modelInstance, next) {
-    if (!utility.JSONIterator(ctx.args.data.category, categoryList))
-      next(new Error('category Validation Error'))
-    if (!utility.JSONIterator(ctx.args.data.userLabel, userLabelList))
-      next(new Error('userLabel Validation Error'))
-    if (!utility.JSONIterator(ctx.args.data.country, countryList))
-      next(new Error('country Validation Error'))
-    if (!utility.JSONIterator(ctx.args.data.language, languageList))
-      next(new Error('language Validation Error'))
-    if (!utility.JSONIterator(ctx.args.data.os, osList))
-      next(new Error('os Validation Error'))
-    if (!utility.JSONIterator(ctx.args.data.connection, deviceList))
-      next(new Error('connection Validation Error'))
-    if (!utility.JSONIterator(ctx.args.data.device, deviceList))
-      next(new Error('device Validation Error'))
-    next()
-  })  
-
 }
--- common/models/subcampaign.js
+++ common/models/subcampaign.js
@@ -1,3 +1,14 @@
+var categoryList = require('../../config/category.json')
+var countryList = require('../../config/country.json')
+var userLabelList = require('../../config/userLabel.json')
+var priorityList = require('../../config/priority.json')
+var languageList = require('../../config/language.json')
+var osList = require('../../config/operatingSystem.json')
+var connectionList = require('../../config/connection.json')
+var deviceList = require('../../config/device.json')
+
+var utility = require('../../public/utility.js')
+
 module.exports = function (subcampaign) {
   
   var subPlan = require('../../config/subPlan')
@@ -6,16 +17,47 @@ module.exports = function (subcampaign) {
   subcampaign.validatesInclusionOf('style', {in: subStyle})
   subcampaign.validatesInclusionOf('plan', {in: subPlan})
 
+  var settingValidator = function (data, callback) {
+    var whiteList = ['priority', 'category', 'country', 'language', 'device', 'os', 'dayParting', 'preferences', 'userLabel', 'connection']
+    if (utility.inputChecker(data, whiteList)) {
+      if (!utility.JSONIterator(data.category, categoryList))
+        callback(new Error('category Validation Error'), null)
+      if (!utility.JSONIterator(data.userLabel, userLabelList))
+        callback(new Error('userLabel Validation Error'), null)
+      if (!utility.JSONIterator(data.country, countryList))
+        callback(new Error('country Validation Error'), null)
+      if (!utility.JSONIterator(data.language, languageList))
+        callback(new Error('language Validation Error'), null)
+      if (!utility.JSONIterator(data.os, osList))
+        callback(new Error('os Validation Error'), null)
+      if (!utility.JSONIterator(data.connection, deviceList))
+        callback(new Error('connection Validation Error'), null)
+      if (!utility.JSONIterator(data.device, deviceList))
+        callback(new Error('device Validation Error'), null)
+      callback(null, 'validated successfully')
+    }
+    else
+      return next(new Error('White List Error! Allowed Parameters: ' + whiteList.toString()))    
+  }
+
   subcampaign.beforeRemote('prototype.__create__setting', function (ctx, modelInstance, next) {
     if (!ctx.args.options.accessToken)
       return next()
     ctx.args.data.clientId = ctx.args.options.accessToken.userId
-    return next()
+    settingValidator(ctx.args.data, function (err, result) {
+      if (err)
+        return next(err)
+      return next()
+    })
   })
 
-  subcampaign.beforeRemote('prototype.__updateById__setting', function (ctx, modelInstance, next) {
+  subcampaign.beforeRemote('prototype.__update__setting', function (ctx, modelInstance, next) {
     if (!ctx.args.options.accessToken)
       return next()
-    return next()
+    settingValidator(ctx.args.data, function (err, result) {
+      if (err)
+        return next(err)
+      return next()
+    })
   })
 }
