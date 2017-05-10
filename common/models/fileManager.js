ce3b8903109ff0a8d97b791c9c3aa892b96202bc
MrWooJ <hurricanc@gmail.com>
2017-05-10 13:19:45 +0430

    Change StaticFile Model Name to File Manager to Handle Data/File Entry

--- common/models/fileManager.js
+++ common/models/fileManager.js
@@ -1,28 +1,26 @@
-'use strict'
 var statusJson = require('../../config/status')
 var CONTAINERS_URL = '/api/containers/'
 
-module.exports = function (StaticFile) {
+module.exports = function (fileManager) {
 
-  StaticFile.uploadFile = function (ctx, options, cb) {
-    StaticFile.app.models.container.upload(ctx.req, ctx.result, options, function (err, fileObj) {
+  fileManager.uploadFile = function (ctx, options, cb) {
+    fileManager.app.models.container.upload(ctx.req, ctx.result, options, function (err, fileObj) {
       if (err)
         return cb(err)
       else {
-        // Here myFile is the field name associated with upload. You should change it to something else if you
-        var fileInfo = fileObj.files.myFile[0]
-        StaticFile.create({
+        var fileInfo = fileObj.files.file[0]
+        fileManager.create({
           name: fileInfo.name,
           type: fileInfo.type,
           container: fileInfo.container,
           clientId: ctx.req.accessToken.userId,
-          url: CONTAINERS_URL + fileInfo.container + '/download/' + fileInfo.name // This is a hack for creating links
+          url: CONTAINERS_URL + fileInfo.container + '/download/' + fileInfo.name
         }, function (err, obj) {
           if (err) {
             console.log('Error in uploading' + err)
             return cb(err)
           } else {
-            var subcampaign = StaticFile.app.models.subcampaign
+            var subcampaign = fileManager.app.models.subcampaign
             subcampaign.findById(options.subcampaignId, function (err, subcampaignInst) {
               if (err)
                 throw err
@@ -38,7 +36,7 @@ module.exports = function (StaticFile) {
     })
   }
 
-  StaticFile.remoteMethod(
+  fileManager.remoteMethod(
     'uploadFile', {
       description: 'Uploads a file',
       accepts: [{
@@ -65,6 +63,5 @@ module.exports = function (StaticFile) {
         verb: 'post'
       }
     }
-
   )
 }
