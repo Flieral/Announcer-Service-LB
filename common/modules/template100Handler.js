eb7727d2922f4eda310318b119fb435bfcfdc00c
Alirëza WJ Arabi <hurricanc@gmail.com>
2017-06-03 16:57:38 +0430

    Implement Input Validator and Merge Data for Template100 Handler

--- common/modules/template100Handler.js
+++ common/modules/template100Handler.js
@@ -1,9 +1,18 @@
 module.exports = {
-  inputValidator: function (fileInfo, callback) {
-
+  inputValidator: function (dataInput, callback) {
+    if (dataInput)
+      if (dataInput.header && dataInput.time && dataInput.holding && dataInput.subtitle)
+        return callback(null)
+    return callback(new Error('Input Validation for Dynamic Template Handler 100 Failed'))
   },
 
   mergeDataWithTemplate: function(data, template, callback) {
-
+    const cheerio = require('cheerio')
+    const $ = cheerio.load(template)
+    $('FL_Header').text(data.header)
+    $('FL_Holding').text(data.holding)
+    $('FL_Time').text(data.time)
+    $('FL_Subtitle').text(data.subtitle)
+    return callback(null, $.html())
   }
 }  
